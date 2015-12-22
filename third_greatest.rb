fadc8801cfa8142f3560084e4df6b9643fb5665f
SiouxsieAsylum <mckenzie.andrea.m@gmail.com>
2015-12-22 15:51:49 -0500

    Create third_greatest.rb

---
+++ third_greatest.rb
@@ -0,0 +1,10 @@
+# Write a method that takes an array of numbers in. Your method should
+# return the third greatest number in the array. You may assume that
+# the array has at least three numbers in it.
+#
+# Difficulty: medium.
+
+def third_greatest(nums)
+  array = nums.sort.uniq
+   array[-3]
+end
